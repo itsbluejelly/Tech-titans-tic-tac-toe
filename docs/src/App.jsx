81a25de9ad37294acac451c30641643159c7e1fa
itsbluejelly <ronniedenzel0@gmail.com>
2023-06-30 15:22:26 +0300

    added gamecell component

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -5,6 +5,7 @@ import Button from "./components/Button"
 import ProgressBar from "./components/ProgressBar"
 import HomePage from "./components/HomePage"
 import PopupText from "./components/PopupText"
+import GameCell from "./components/GameCell"
 
 export default function App(){
     return(
@@ -15,47 +16,21 @@ export default function App(){
             {/* THE GAME CONTAINER WHICH HOLDS THE GRID OF CELLS AND THE SELECTION BOX IS PLACED HERE */}
             <div className="dark:bg-[#2B0040] h-[95vh] rounded-[50px] w-[95%] relative bg-[limegreen] flex justify-center items-center flex-col">
                 {/* THE DIV SHOWING THE CURRENT PLAYER */}
-                <ProgressBar/>
+                {/* <ProgressBar/> */}
 
                 {/* THE SELECTION MENU IS CONTAINED HERE */}
                 <HomePage/>  
 
                 {/* THE CONTAINER FOR GRID IS CONTAINED HERE */}
-                <div className="grid grid-flow-row grid-cols-3 gap-[10px] transition-all duration-500">
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                    <div 
-                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
-                    </div>
-                </div>
+                {/* <div className="grid grid-flow-row grid-cols-3 gap-[10px] transition-all duration-500">
+                    <GameCell/>
+                </div> */}
 
                 {/* THE BUTTON IS LOCATED HERE TO START OR RESET GAME*/}
                 <Button/>
 
                 {/* THE DISPLAY OF A WINNER SHOWN, OR THE PLAYER */}
-                <PopupText/>
+                {/* <PopupText/> */}
 
                 {/* THE GAME NAME BOTTOM RIGHT FOOTER IS PLACED HERE */}
                 <footer>
---
+++ docs/src/components/GameCell.jsx
@@ -0,0 +1,7 @@
+export default function GameCell(){
+    return(
+        <div 
+            className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700] transition-all duration-500 hover:shadow-popup">1
+        </div>
+    )
+}
