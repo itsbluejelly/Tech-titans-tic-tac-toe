8d804689ef363b3efa22cde692dbbe843d113609
itsbluejelly <ronniedenzel0@gmail.com>
2023-06-30 13:16:10 +0300

    added game grid with styling

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -4,17 +4,51 @@ import CreditFooter from "./components/CreditsFooter"
 
 export default function App(){
     return(
-        // THE WHOLE GAME, INCLUDING THE GRID AND THE SELECTION BOX IS INT THIS CONTAINER BELOW
+        // THE WHOLE GAME IS IN THIS CONTAINER BELOW
         <main 
-            className="min-h-[100vh] bg-[#59E391] dark:bg-[#5A1E76] relative font-['Fredoka'] transition-all duration-500"
+            className="min-h-[100vh] bg-[#59E391] dark:bg-[#5A1E76] font-['Fredoka'] transition-all duration-500 flex justify-center items-center "
         >
-            {/* THE GAME NAME BOTTOM RIGHT FOOTER IS PLACED HERE */}
-            <footer>
-                {/* THIS IS THE NAME FOOTER */}
-                <NameFooter/>
-                {/* THE CREDITS FOOTER IS PLACED HERE */}
-                <CreditFooter/>
-            </footer>
+            {/* THE GAME CONTAINER WHICH HOLDS THE GRID OF CELLS AND THE SELECTION BOX IS PLACED HERE */}
+            <div className="dark:bg-[#2B0040] h-[95vh] rounded-[50px] w-[95%] relative bg-[limegreen] flex justify-center items-center flex-col">
+                {/* THE CONTAINER FOR GRID IS CONTAINED HERE */}
+                <div className="grid grid-flow-row grid-cols-3 gap-[10px]">
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    <div 
+                        className="flex justify-center items-center w-[100px] h-[100px] dark:bg-[#43115B] rounded-[10px] dark:text-[#DCBF3F] bg-[#59E391] text-cyan-700 text-xl font-[700]">1
+                    </div>
+                    
+                </div>
+                {/* THE GAME NAME BOTTOM RIGHT FOOTER IS PLACED HERE */}
+                <footer>
+                    {/* THIS IS THE NAME FOOTER */}
+                    <NameFooter/>
+                    {/* THE CREDITS FOOTER IS PLACED HERE */}
+                    <CreditFooter/>
+                </footer>
+            </div>
         </main>
     )
 }
--- docs/src/components/NameFooter.jsx
+++ docs/src/components/NameFooter.jsx
@@ -1,12 +1,12 @@
 export default function NameFooter(){
     return(
         <div
-            className="absolute bottom-0 left-0 font-['Fredoka'] text-7xl font-[600] leading-[0.8]"
+            className="absolute bottom-[10px] left-[15px] font-['Fredoka'] text-5xl font-[600] leading-[0.8]"
         >
             <p
                 className="dark:text-[#DCBF3F] text-cyan-600">tic.
             </p>
-            
+
             <p
                 className="dark:text-[#72CFF9] text-[#ffff00f5]">tac.
             </p>
