dec4e0a9ccdb051d7ba57a10fc834740337e8f01
iamfrankmunene <134502286+iamfrankmunene@users.noreply.github.com>
2023-06-30 07:54:31 +0300

    design of the game grid

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -5,12 +5,17 @@ const options = ["","","","","","","","",""]
 function App() {
   const [ game, setGame ] = React.useState(options)
   return (
-       <div className="container mb-4 p-4 rounded-2xl bg-white grid grid-flow-row grid-cols-3 w-[300px] h-[300px] gap-2">
-         {game.map((player, index) => (
-          <div key={index} className="border-black border-[2px] rounded-lg flex justify-center items-center bg-blue-300 font-semibold text-3xl">{player}</div>
-         ))}
-       </div> 
-  )
+    <section>
+        <div className="container mb-4 p-4 rounded-2xl bg-gradient-to-br from-orange-400 to-orange-200 grid grid-flow-row grid-cols-3 w-[350px] h-[350px] gap-2 shadow-lg">
+          {game.map((player, index) => (
+            <div key={index} className="rounded-lg flex justify-center items-center  font-semibold text-3xl shadow-inner bg-gray-200">{player}</div>
+          ))}
+        </div>
+        <footer class="text-xl font-bold pt-5 text-center text-white">
+          &copy;By:The Tech Titans
+          </footer>
+       </section>
+  ) 
 }
 
 export default App
