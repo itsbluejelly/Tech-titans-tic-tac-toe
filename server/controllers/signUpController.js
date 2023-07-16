e6c71a261332f9b6b678218becf525eda985d43a
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-17 02:18:04 +0300

    added signUp controller

---
+++ server/controllers/signUpController.js
@@ -0,0 +1,8 @@
+const eventLogger = require('../middleware/eventLogger')
+const UserModel = require('../model/User')
+
+async function signUpController(req, res, next){
+    next()
+}
+
+module.exports = signUpController
